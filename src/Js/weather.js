17bc9f2964e5f0e3971b04c1326127746831a282
sejalgoyal12341234-create <sejalgoyal12341234@gmail.com>
2025-12-25 10:04:25 +0530

    day-6 done

--- src/Js/weather.js
+++ src/Js/weather.js
@@ -5,6 +5,13 @@ const cityName = document.querySelector("#cityName");
 const loading = document.querySelector("#loading");
 const errorBox = document.querySelector("#error");
 
+const weatherBox = document.querySelector("#weatherBox");
+const temp = document.querySelector("#temp");
+const feelsLike = document.querySelector("#feels");
+const condition = document.querySelector("#desc");
+const humidity = document.querySelector("#humidity");
+const windSpeed = document.querySelector("#wind");
+
 let API_KEY = "6394e37da81b77b2ed8ba52ba425e1ee";
 
 if (!city) {
@@ -18,7 +25,7 @@ if (!city) {
       const geoRes = await fetch(
         `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${API_KEY}`
       );
-      console.log(geoRes);
+      // console.log(geoRes);
 
       if (!geoRes.ok) throw new error("Geo API failed");
 
@@ -27,6 +34,25 @@ if (!city) {
 
       let { lat, lon } = geoData[0];
       console.log(lat, lon);
+
+      const weatherRes = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
+      );
+      // console.log(weatherRes);
+
+      if (!weatherRes.ok) throw new Error("Weather API failed");
+
+      const weatherData = await weatherRes.json();
+      // console.log(weatherData);
+
+      temp.textContent = weatherData.main.temp;
+      feelsLike.textContent = weatherData.main.feels_like;
+      condition.textContent = weatherData.weather[0].description;
+      humidity.textContent = weatherData.main.humidity;
+      windSpeed.textContent = weatherData.wind.windSpeed;
+
+      loading.classList.add("hidden");
+      weatherBox.classList.remove("hidden");
     } catch (error) {
       console.log(error, "Error in fetching weather details");
       loading.classList.add("hidden");
